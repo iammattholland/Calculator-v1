c3a5e7649450ae87a3fa6ce588624dc245befd69
Matt Holland <mattsignin.1@live.com>
2020-08-27 08:53:52 -0400

    %got the calc key to show on screen

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-var screen = 0;
+//Calculator Javascript
 
 function calc(press) {
 
-    var screen = press;
+    var calcScreen = press;
+
+}
+
+//Shows key value on calculator screen
+function press(key) {
+    var calcScreen = document.getElementById("calculator-screen");
+    calcScreen.value = key;
+    
+}
+
+
+
+function clear() {
+
+    var calcScreen = 0;
+    var total = 0;
+
+}
+
+
+
+function equals() {
+
+return total;
 
 }
