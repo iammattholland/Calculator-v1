052de45a03da0849af60d7469fe51d1e8e73bcd6
Matt Holland <mattsignin.1@live.com>
2020-09-02 14:04:09 -0400

    using some online code to try and learn

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //Calculator Javascript
 
+/*
 
 //Does the calculation
 function calc(press) {
@@ -14,6 +15,7 @@ function calc(press) {
 function press(key) {
     var calcScreen = document.getElementById("calculator-screen");
     calcScreen.value = key;
+
     
 }
 
@@ -32,3 +34,51 @@ function equals() {
     return totalClac;
 
 }
+*/
+
+// From Online to try https://freshman.tech/calculator/
+
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+
+function updateDisplay() {
+    // select the element with class of `calculator-screen`
+    const display = document.querySelector('.calculator-screen');
+    // update the value of the element with the contents of `displayValue`
+    display.value = calculator.displayValue;
+  }
+  
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+  // Access the clicked element
+  const { target } = event;
+
+  // Check if the clicked element is a button.
+  // If not, exit from the function
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  console.log('digit', target.value);
+});
